f86e9dc7812caa8d6783e1e14f53eb05dbca5903
Herbert Nícolas <hna@cin.ufpe.br>
2021-09-15 10:55:14 -0300

    Criação da estrutura do projeto .NET

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        int numeroDeVezes = 5;
+        static void Main(string[] args)
+        {
+            
+            for(int i =0; i<numeroDeVezes; i++){
+                Console.WriteLine($"Hello World! {i}");
+            }
+        }
+    }
+}
